9f28f89f2f6bb0b4df1062deb5ea06497993f283
adumbidiot <nathaniel.daniel23@outlook.com>
2020-10-16 13:51:11 -0700

    Update to git winrt

--- windows_media_transcoding_bindings/build.rs
+++ windows_media_transcoding_bindings/build.rs
@@ -1,10 +1,6 @@
-winrt::build!(
-    dependencies
-        nuget: Microsoft.Windows.SDK.Contracts
-    types
-        windows::media::transcoding::*
-);
-
 fn main() {
-    build();
+    winrt::build!(
+        types
+            windows::media::transcoding::*
+    );
 }
